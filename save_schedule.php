8c7bd99c3d1e6049b35fac4fbad1bab16498352a
lucidojm <lucidojohncarlo10@gmail.com>
2024-06-26 02:23:26 +0800

    6-26-24

--- save_schedule.php
+++ save_schedule.php
@@ -1,6 +1,10 @@
 <?php 
 require_once('db-connect.php');
-
+if($_SERVER['REQUEST_METHOD'] !='POST'){
+    echo "<script> alert('Error: No data to save.'); location.replace('./') </script>";
+    $conn->close();
+    exit;
+}
 extract($_POST);
 $allday = isset($allday);
 
