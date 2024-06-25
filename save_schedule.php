f616dc34e48f5b6f3e402fb4416cad056f411230
lucidojm <lucidojohncarlo10@gmail.com>
2024-06-26 02:36:43 +0800

    6-26-24

--- save_schedule.php
+++ save_schedule.php
@@ -1,27 +1,32 @@
-<?php 
+<?php
 require_once('db-connect.php');
-if($_SERVER['REQUEST_METHOD'] !='POST'){
+
+if ($_SERVER['REQUEST_METHOD'] != 'POST') {
     echo "<script> alert('Error: No data to save.'); location.replace('./') </script>";
     $conn->close();
     exit;
 }
-extract($_POST);
-$allday = isset($allday);
 
-if(empty($id)){
-    $sql = "INSERT INTO `schedule_list` (`title`,`description`,`start_datetime`,`end_datetime`) VALUES ('$title','$description','$start_datetime','$end_datetime')";
-}else{
-    $sql = "UPDATE `schedule_list` set `title` = '{$title}', `description` = '{$description}', `start_datetime` = '{$start_datetime}', `end_datetime` = '{$end_datetime}' where `id` = '{$id}'";
+extract($_POST);
+
+$id = isset($id) ? (int)$id : null;
+
+if (empty($id)) {
+    // Assuming `id` is auto-incremented, we don't include it in the insert statement
+    $sql = "INSERT INTO `schedule_list` (`title`, `description`, `start_datetime`, `end_datetime`) VALUES ('$title', '$description', '$start_datetime', '$end_datetime')";
+} else {
+    $sql = "UPDATE `schedule_list` SET `title` = '{$title}', `description` = '{$description}', `start_datetime` = '{$start_datetime}', `end_datetime` = '{$end_datetime}' WHERE `id` = '{$id}'";
 }
-$save = $conn->query($sql);
-if($save){
+
+if ($conn->query($sql)) {
     echo "<script> alert('Schedule Successfully Saved.'); location.replace('./') </script>";
-}else{
+} else {
     echo "<pre>";
-    echo "An Error occured.<br>";
-    echo "Error: ".$conn->error."<br>";
-    echo "SQL: ".$sql."<br>";
+    echo "An Error occurred.<br>";
+    echo "Error: " . $conn->error . "<br>";
+    echo "SQL: " . $sql . "<br>";
     echo "</pre>";
 }
+
 $conn->close();
 ?>
